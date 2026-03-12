78eac862132ed19d1c10cb8702daee5644d1e79f
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-03-11 22:14:15 -0300

    Mapeando classes entidades
    Enum , @Column nomeando para o banco de dados

---
+++ modelos-empresas-erp-jsf-primefaces/src/main/java/com/medeirossoftware/erp/model/TipoEmpresa.java
@@ -0,0 +1,27 @@
+package com.medeirossoftware.erp.model;
+
+public enum TipoEmpresa {
+
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+}
